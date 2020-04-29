6d00d0d2e06269ce61de5d1a12d3c74454d44149
Denis Yarats <denisyarats@gmail.com>
2020-04-28 18:34:59 -0700

    init

---
+++ README.md
@@ -0,0 +1,81 @@
+# DrQ: Data regularized Q
+
+This is a PyTorch implementation of **DrQ** from
+
+**Image Augmentation Is All You Need: Regularizing Deep Reinforcement Learning from Pixels** by
+
+[Denis Yarats*](https://cs.nyu.edu/~dy1042/), [Ilya Kostrikov*](https://github.com/ikostrikov), [Rob Fergus](https://cs.nyu.edu/~fergus/pmwiki/pmwiki.php).
+
+*Equal contribution. Author ordering determined by coin flip.
+
+[[Paper]](https://arxiv.org/abs/2004.13649) [[Webpage]](https://sites.google.com/view/data-regularized-q)
+
+## Citation
+If you use this repo in your research, please consider citing the paper as follows
+```
+@article{kostrikov2020image,
+    title={Image Augmentation Is All You Need: Regularizing Deep Reinforcement Learning from Pixels},
+    author={Ilya Kostrikov and Denis Yarats and Rob Fergus},
+    year={2020},
+    eprint={2004.13649},
+    archivePrefix={arXiv},
+    primaryClass={cs.LG}
+}
+```
+
+## Requirements
+We assume you have access to a gpu that can run CUDA 9.2. Then, the simplest way to install all required dependencies is to create an anaconda environment by running
+```
+conda env create -f conda_env.yml
+```
+After the instalation ends you can activate your environment with
+```
+source activate drq
+```
+
+## Instructions
+To train the DrQ agent on the `Cheetah Run` task run
+```
+python train.py env=cheetah_run
+```
+This will produce the `runs` folder, where all the outputs are going to be stored including train/eval logs, tensorboard blobs, and evaluation episode videos. To launch tensorboard run
+```
+tensorboard --logdir runs
+```
+
+The console output is also available in a form:
+```
+| train | E: 5 | S: 5000 | R: 11.4359 | D: 66.8 s | BR: 0.0581 | ALOSS: -1.0640 | CLOSS: 0.0996 | TLOSS: -23.1683 | TVAL: 0.0945 | AENT: 3.8132
+```
+a training entry decodes as
+```
+train - training episode
+E - total number of episodes 
+S - total number of environment steps
+R - episode return
+D - duration in seconds
+BR - average reward of a sampled batch
+ALOSS - average loss of the actor
+CLOSS - average loss of the critic
+TLOSS - average loss of the temperature parameter
+TVAL - the value of temperature
+AENT - the actor's entropy
+```
+while an evaluation entry
+```
+| eval  | E: 20 | S: 20000 | R: 10.9356
+```
+contains 
+```
+E - evaluation was performed after E episodes
+S - evaluation was performed after S environment steps
+R - average episode return computed over `num_eval_episodes` (usually 10)
+```
+
+## The PlaNet Benchmark
+**DrQ** demonstrates the state-of-the-art performance on a set of challenging image-based tasks from the DeepMind Control Suite (Tassa et al., 2018). We compare against PlaNet (Hafner et al., 2018), SAC-AE (Yarats et al., 2019), SLAC (Lee et al., 2019), CURL (Srinivas et al., 2020), and an upper-bound performance SAC States (Haarnoja et al., 2018). This follows the benchmark protocol established in PlaNet (Hafner et al., 2018).
+![The PlaNet Benchmark](graphs/planet_bench.png)
+
+## The Dreamer Benchmark
+**DrQ** demonstrates the state-of-the-art performance on an extended set of challenging image-based tasks from the DeepMind Control Suite (Tassa et al., 2018), following the benchmark protocol from Dreamer (Hafner et al., 2019). We compare against Dreamer (Hafner et al., 2019) and an upper-bound performance SAC States (Haarnoja et al., 2018).
+![The Dreamer Benchmark](graphs/dreamer_bench.png)
---
+++ train.py
@@ -0,0 +1,178 @@
+import copy
+import math
+import os
+import pickle as pkl
+import sys
+import time
+
+import numpy as np
+
+import dmc2gym
+import hydra
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import utils
+from logger import Logger
+from replay_buffer import ReplayBuffer
+from video import VideoRecorder
+
+torch.backends.cudnn.benchmark = True
+
+
+def make_env(cfg):
+    """Helper function to create dm_control environment"""
+    if cfg.env == 'ball_in_cup_catch':
+        domain_name = 'ball_in_cup'
+        task_name = 'catch'
+    elif cfg.env == 'point_mass_easy':
+        domain_name = 'point_mass'
+        task_name = 'easy'
+    else:
+        domain_name = cfg.env.split('_')[0]
+        task_name = '_'.join(cfg.env.split('_')[1:])
+
+    # per dreamer: https://github.com/danijar/dreamer/blob/02f0210f5991c7710826ca7881f19c64a012290c/wrappers.py#L26
+    camera_id = 2 if domain_name == 'quadruped' else 0
+
+    env = dmc2gym.make(domain_name=domain_name,
+                       task_name=task_name,
+                       seed=cfg.seed,
+                       visualize_reward=False,
+                       from_pixels=True,
+                       height=cfg.image_size,
+                       width=cfg.image_size,
+                       frame_skip=cfg.action_repeat,
+                       camera_id=camera_id)
+
+    env = utils.FrameStack(env, k=cfg.frame_stack)
+
+    env.seed(cfg.seed)
+    assert env.action_space.low.min() >= -1
+    assert env.action_space.high.max() <= 1
+
+    return env
+
+
+class Workspace(object):
+    def __init__(self, cfg):
+        self.work_dir = os.getcwd()
+        print(f'workspace: {self.work_dir}')
+
+        self.cfg = cfg
+
+        self.logger = Logger(self.work_dir,
+                             save_tb=cfg.log_save_tb,
+                             log_frequency=cfg.log_frequency_step,
+                             agent=cfg.agent.name,
+                             action_repeat=cfg.action_repeat)
+
+        utils.set_seed_everywhere(cfg.seed)
+        self.device = torch.device(cfg.device)
+        self.env = make_env(cfg)
+
+        cfg.agent.params.obs_shape = self.env.observation_space.shape
+        cfg.agent.params.action_shape = self.env.action_space.shape
+        cfg.agent.params.action_range = [
+            float(self.env.action_space.low.min()),
+            float(self.env.action_space.high.max())
+        ]
+        self.agent = hydra.utils.instantiate(cfg.agent)
+
+        self.replay_buffer = ReplayBuffer(self.env.observation_space.shape,
+                                          self.env.action_space.shape,
+                                          cfg.replay_buffer_capacity,
+                                          self.cfg.image_pad, self.device)
+
+        self.video_recorder = VideoRecorder(
+            self.work_dir if cfg.save_video else None)
+        self.step = 0
+
+    def evaluate(self):
+        average_episode_reward = 0
+        for episode in range(self.cfg.num_eval_episodes):
+            obs = self.env.reset()
+            self.video_recorder.init(enabled=(episode == 0))
+            done = False
+            episode_reward = 0
+            episode_step = 0
+            while not done:
+                with utils.eval_mode(self.agent):
+                    action = self.agent.act(obs, sample=False)
+                obs, reward, done, info = self.env.step(action)
+                self.video_recorder.record(self.env)
+                episode_reward += reward
+                episode_step += 1
+
+            average_episode_reward += episode_reward
+            self.video_recorder.save(f'{self.step}.mp4')
+        average_episode_reward /= self.cfg.num_eval_episodes
+        self.logger.log('eval/episode_reward', average_episode_reward,
+                        self.step)
+        self.logger.dump(self.step)
+
+    def run(self):
+        episode, episode_reward, episode_step, done = 0, 0, 1, True
+        start_time = time.time()
+        while self.step < self.cfg.num_train_steps:
+            if done:
+                if self.step > 0:
+                    self.logger.log('train/duration',
+                                    time.time() - start_time, self.step)
+                    start_time = time.time()
+                    self.logger.dump(
+                        self.step, save=(self.step > self.cfg.num_seed_steps))
+
+                # evaluate agent periodically
+                if self.step % self.cfg.eval_frequency == 0:
+                    self.logger.log('eval/episode', episode, self.step)
+                    self.evaluate()
+
+                self.logger.log('train/episode_reward', episode_reward,
+                                self.step)
+
+                obs = self.env.reset()
+                done = False
+                episode_reward = 0
+                episode_step = 0
+                episode += 1
+
+                self.logger.log('train/episode', episode, self.step)
+
+            # sample action for data collection
+            if self.step < self.cfg.num_seed_steps:
+                action = self.env.action_space.sample()
+            else:
+                with utils.eval_mode(self.agent):
+                    action = self.agent.act(obs, sample=True)
+
+            # run training update
+            if self.step >= self.cfg.num_seed_steps:
+                for _ in range(self.cfg.num_train_iters):
+                    self.agent.update(self.replay_buffer, self.logger,
+                                      self.step)
+
+            next_obs, reward, done, info = self.env.step(action)
+
+            # allow infinite bootstrap
+            done = float(done)
+            done_no_max = 0 if episode_step + 1 == self.env._max_episode_steps else done
+            episode_reward += reward
+
+            self.replay_buffer.add(obs, action, reward, next_obs, done,
+                                   done_no_max)
+
+            obs = next_obs
+            episode_step += 1
+            self.step += 1
+
+
+@hydra.main(config_path='config.yaml', strict=True)
+def main(cfg):
+    from train import Workspace as W
+    workspace = W(cfg)
+    workspace.run()
+
+
+if __name__ == '__main__':
+    main()
